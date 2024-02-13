94c280af15fbee01ac7bba9c3d0b04cdeb5706ed
Max__La__Menace <maxence.depire@gmail.com>
2024-02-13 09:44:13 +0100

    update all files, as a first commit.

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 char version[3] = "0.1";
 
-void color(char code[5]){
+void colorChar(char code[5], char content[256]){
 	if (strcmp(code, "-y") == 0){
 		printf("\033[1;33m");	
 	}
@@ -13,33 +14,65 @@ void color(char code[5]){
 	else if (strcmp(code, "-rs") == 0){
 		printf("\033[0m");
 	}
+	printf("%s", content);
+	printf("\033[0m");
+	return;
+}
+
+void colorInt(char code[5], int content){
+	if (strcmp(code, "-y") == 0){
+		printf("\033[1;33m");	
+	}
+	else if (strcmp(code, "-r") == 0){
+		printf("\033[1;31m");
+	}
+	else if (strcmp(code, "-rs") == 0){
+		printf("\033[0m");
+	}
+	printf("%d", content);
+	printf("\033[0m");
+	return;
 }
 
 void readFile(int argc, char **argv){
 	FILE *file;
 	file = fopen(argv[2], "r");
 	if (file == NULL){
-		color("-r");
-		printf("error in the file open process");
-		color("-rs");
+		colorChar("-r", "error in the file open process");
 		return;
 	}
-	const unsigned MAX_SIZE = 256;
+
+	bool lineNumberShow = false;
+	int lineCount = 1;
+
+	for (int i = 0; i != argc; i++){
+		if (strcmp("-l", argv[i]) == 0){
+			lineNumberShow = true;
+			break;
+		}
+	}
+
+	const unsigned int MAX_SIZE = 256;
 	char buffer[MAX_SIZE];
 	while (fgets(buffer, MAX_SIZE, file)){
+		if (lineNumberShow == true){
+			colorInt("-y", lineCount);
+			printf(" : ");
+			lineCount++;
+		}
 		printf("%s", buffer);
 	}
+	return;
 }
 
 void router(int argc, char **argv){
 	if (argc == 1){
 		printf("for help use : ");
-		color("-r");
-		printf("cfile -help");
+		colorChar("-r", "cfile -h");
 		return;	
 	}
 	else if (argc > 1){
-		if (strcmp(argv[1], "-help") == 0){
+		if (strcmp(argv[1], "-h") == 0){
 			printf("cfile - version %s", version);
 			printf("\n-r : Read file.");
 		}
@@ -47,11 +80,12 @@ void router(int argc, char **argv){
 			readFile(argc, argv);
 		}
 	}
+	return;
 }
 
 
 int main(int argc, char **argv){
-	
 	router(argc, argv);
-	printf("\n");
+	colorChar("-rs", "");
+	return 0;
 }
