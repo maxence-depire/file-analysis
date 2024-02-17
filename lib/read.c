8cc7a7390b55676d2c17cea3733274b8b75eb19c
Maxence Depire <maxence.depire@gmail.com>
2024-02-17 14:36:32 +0100

    lil update on the read function, adding the color mode for special character

--- lib/read.c
+++ lib/read.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdbool.h> 
 
 // READ FUNCTION
 void readFile(int argc, char **argv){
+
 	FILE *file;
 	file = fopen(argv[2], "r");
+
+	char line[1024];
+	int lineCounter = 0;
+
 	if (file == NULL){
 		colorString("-r", "error in the file open process");
 		return;
 	}
 
-	bool lineNumberShow = false;
-	int lineCount = 1;
-	char lineStart[256];
-
-	for (int i = 0; i != argc; i++){
-		if (strcmp("-l", argv[i]) == 0){
-			lineNumberShow = true;
-			break;
+	while (fgets(line, sizeof(line), file) != NULL){
+		printf("\033[1;33m%d :\033[0m ", lineCounter++);
+		for (int i = 0; line[i] != 0; i++) {
+			if (
+				line[i] == '{'
+				|| line[i] == '}'
+				|| line[i] == '('
+				|| line[i] == ')'
+			) {
+				colorChar("-r", line[i]);
+			}
+			else {
+				printf("%c", line[i]);
+			}
 		}
 	}
 
-	const unsigned int MAX_SIZE = 256;
-	char buffer[MAX_SIZE];
-	while (fgets(buffer, MAX_SIZE, file)){
-		if (lineNumberShow == true){
-			sprintf(lineStart, "%d : ", lineCount);
-			colorString("-y", lineStart);
-			lineCount++;
-		}
-		printf("%s", buffer);
-	}
 	return;
 }
 // READ FUNCTION
